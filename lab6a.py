e993c211dc33d7735d48626192305c629a641b8e
yzwai <yzwai@myseneca.ca>
2024-07-19 15:02:34 +0000

    Added displayCourses, 3rd problem solved.

--- lab6a.py
+++ lab6a.py
@@ -6,7 +6,7 @@ class Student:
     # Define the name and number when a student object is created, ex. student1 = Student('john', 025969102)
     def __init__(self, name, number):
         self.name = name
-        self.number = number
+        self.number = str(number)
         self.courses = {}
 
     # Return student name and number
