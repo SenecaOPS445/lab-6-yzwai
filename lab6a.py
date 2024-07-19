3134b14a324946119c46dc125a6ba3b9f37a07c1
yzwai <yzwai@myseneca.ca>
2024-07-19 15:05:59 +0000

    1st problem solved.

--- lab6a.py
+++ lab6a.py
@@ -6,7 +6,7 @@ class Student:
     # Define the name and number when a student object is created, ex. student1 = Student('john', 025969102)
     def __init__(self, name, number):
         self.name = name
-        self.number = str(number)
+        self.number = str(number) #1st problem, number would always be a string
         self.courses = {}
 
     # Return student name and number
